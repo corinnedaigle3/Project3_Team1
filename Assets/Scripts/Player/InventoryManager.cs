ed10997018cf2f3f0c08d5f8f34524737e7501be
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-03-25 19:41:08 -0400

    InventoryScriptUpdate

---
+++ Assets/Scripts/Player/InventoryManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public InventorySlot[] inventorySlots;
+    public GameObject inventoryItemPrefab;
+
+    private int selectedSlot = -1;
+
+    private void Start()
+    {
+        ChangeSelectedSlot(0);
+    }
+
+    private void Update()
+    {
+        if (Input.inputString != null)
+        {
+            bool isNumber = int.TryParse(Input.inputString, out int number);
+            if (isNumber && number > 0 && number < 4)
+            {
+                ChangeSelectedSlot(number - 1);
+            }
+        }
+    }
+
+    void ChangeSelectedSlot(int newValue)
+    {
+        if (selectedSlot >= 0)
+        {
+            inventorySlots[selectedSlot].Deselect();
+        }
+
+        inventorySlots[newValue].Select();
+        selectedSlot = newValue;
+    }
+
+    public void AddItem(Item item)
+    {
+        //Find any empty slot
+        for (int i = 0; i < inventorySlots.Length; i++) 
+        { 
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemSlot != null)
+            {
+                SpawnNewItem(item, slot);
+                return;
+            }
+        }
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot)
+    {
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitialiseItem(item);
+    }
+
+    public Item GetSelectedItem()
+    {
+        InventorySlot slot = inventorySlots[selectedSlot];
+        InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+        if(itemInSlot != null)
+        {
+            return itemInSlot.item;
+        }
+
+        return null;
+    }
+}
