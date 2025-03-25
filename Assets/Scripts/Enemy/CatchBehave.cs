35e2b86efd5afeda291c7f7d90f258f6bc6ab1fe
popoyo07 <piwedaa@gmail.com>
2025-03-24 21:38:30 -0400

    fixed invisibility

---
+++ Assets/Scripts/Enemy/CatchBehave.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchBehave : MonoBehaviour
+{
+    public GameObject theCatch;
+    public bool playerBehind =false;
+    public PlayerMovement p;
+    
+    void Start()
+    {
+        p = GameObject.Find("Player").GetComponent<PlayerMovement>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (p.alreadyInvisible)
+        {
+            theCatch.SetActive(false);
+          
+        }
+        else if (!p.alreadyInvisible)
+        {
+            theCatch.SetActive(true);
+        }
+    }
+
+
+}
