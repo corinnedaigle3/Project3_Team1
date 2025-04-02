77f9ea4ff93bb8b0868ec625e41942b85868a452
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-04-02 19:43:53 -0400

    GameManager

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HubManager : MonoBehaviour
+public class GameManger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
