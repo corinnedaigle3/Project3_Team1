66f88b3f3cff452d83c88e5a42f17027eb07b9e3
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-04-02 20:25:32 -0400

    u

--- Assets/Scripts/ElysiumManager.cs
+++ Assets/Scripts/ElysiumManager.cs
@@ -12,15 +12,14 @@ public class ElysiumManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(TakeDownItemE, new Vector3(), Quaternion.identity);
-        Instantiate(TakeDownItemE, new Vector3(), Quaternion.identity);
-        Instantiate(TakeDownItemE, new Vector3(), Quaternion.identity);
+        Instantiate(TakeDownItemE, new Vector3(-97.12f, 3.99f, -128.56f), Quaternion.identity);
+        Instantiate(TakeDownItemE, new Vector3(132.55f, 2.65f, -7.14f), Quaternion.identity);
+        Instantiate(TakeDownItemE, new Vector3(156.02f, 8f, -129.34f), Quaternion.identity);
 
-        Instantiate(Helm, new Vector3(), Quaternion.identity);
-        Instantiate(Helm, new Vector3(), Quaternion.identity);
+        Instantiate(Helm, new Vector3(153.38f, 9.87f, -69.08f), Quaternion.identity);
+        Instantiate(Helm, new Vector3(44.77f, 4.07f, -12.17f), Quaternion.identity);
     }
-
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
         
