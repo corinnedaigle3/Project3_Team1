1b837b47eea261ae57e019968bc1d8604aa773ff
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-04-09 18:45:23 -0400

    fixed update for camera

--- Assets/Scripts/Player/Cam.cs
+++ Assets/Scripts/Player/Cam.cs
@@ -40,7 +40,7 @@ public class Cam : MonoBehaviour
 
         if (inputDir != Vector3.zero)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.fixedDeltaTime * rotationSpeed);
         }
 
         MyInput();
