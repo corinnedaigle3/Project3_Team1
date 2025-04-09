9d715a4f8ab38630cb93a03be00395fe7eed882f
Sze <sz982600@ucf.edu>
2025-04-09 04:20:01 -0400

    update MY LEVEL

--- Assets/Scripts/Environment/DirectionLight.cs
+++ Assets/Scripts/Environment/DirectionLight.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class DirectionLight : MonoBehaviour
 {
+    [Header("Light Settings")]
     public Light Light;
     public bool LightDisabled;
+
+    [Header("Waters Settings")]
+    public Material Material_Original;
+    public Material Material_Changed;
+    public GameObject Object;
+
 
     private void Start()
     {
         RenderSettings.fogColor = new Color(1f, 0.7450980392156863f, 0.35294117647058826f);
         RenderSettings.fog = enabled;
+        RenderSettings.fogDensity = 0.005f;
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -19,7 +28,11 @@ public class DirectionLight : MonoBehaviour
         {
             LightDisabled = true;
             Light.intensity = 0;
+
+            Object.GetComponent<MeshRenderer>().material = Material_Changed;
+
             RenderSettings.fogColor = new Color(1f, 0f, 0f);
+            RenderSettings.fogDensity = 0.001f;
 
         }
     }
@@ -30,7 +43,11 @@ public class DirectionLight : MonoBehaviour
         {
             LightDisabled = false;
             Light.intensity = 1;
+            Object.GetComponent<MeshRenderer>().material = Material_Original;
+
+
             RenderSettings.fogColor = new Color(1f, 0.7450980392156863f, 0.35294117647058826f);
+            RenderSettings.fogDensity = 0.005f;
 
         }
     }
