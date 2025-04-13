3e496b2f236742ac92371e7fa2c3a1277f2a548d
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-04-13 16:10:32 -0400

    RemoveCaughtTimer

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -91,8 +91,7 @@ public class PlayerMovement : MonoBehaviour
     public gemsChecker theGemChecker;
     private bool canDodge;
     public bool dashUnlocked;
-    public float caughtTimer;
-
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -197,12 +196,6 @@ public class PlayerMovement : MonoBehaviour
                     inventoryManager.invisText.SetActive(false);
                 }
 
-                if (caughtTimer >= 0)
-                {
-                    lose = true;
-                    gameObject.SetActive(false);
-                }
-
                 //check if player is on ground
                 if (grounded && dashUnlocked == true)
                 {
@@ -215,7 +208,6 @@ public class PlayerMovement : MonoBehaviour
                 }
 
                 invisibleTimer -= Time.deltaTime;
-                caughtTimer -= Time.deltaTime;
                 break;
 
             case State.Rolling:
@@ -367,13 +359,14 @@ public class PlayerMovement : MonoBehaviour
         if (other.CompareTag("EnemyE") || other.CompareTag( "Fury") || other.CompareTag("EnemyA") || other.CompareTag("EnemyA"))
         {
             caughtSound.Play();
+            lose = true;
             inventoryManager.takeDowntext.SetActive(false);
             inventoryManager.invisText.SetActive(false);
             inventoryManager.dodgeText.SetActive(false);
             inventoryManager.helmUseText.SetActive(false);
-            caughtTimer = 1f;
             other.gameObject.GetComponentInParent<EnemyBehavior>().playerLose = true;
             transform.LookAt(other.transform.position); // might remvoe it 
+            gameObject.SetActive(false);
         }
 
         if (other.tag == "DashUnlocked")
