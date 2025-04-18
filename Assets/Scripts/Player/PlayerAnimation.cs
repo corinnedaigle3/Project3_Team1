afb57295b5d0b02716f7cab6663ae8c1a3653660
LilyW3865 <li733414@ucf.edu>
2025-04-18 04:34:46 -0400

    Player Animation Controller Added

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{  // PlayerMovement playerMovement;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if() //if player is moving set walking animation to true
+        {
+            Debug.Log("Player moving");
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            Debug.Log("Player not moving");
+            animator.SetBool("walking", false);
+        }*/
+        
+    }
+}
