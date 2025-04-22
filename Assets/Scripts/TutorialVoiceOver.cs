20ce1157a735f9089392777ca728f58a3aa12a8a
LilyW3865 <li733414@ucf.edu>
2025-04-22 18:38:37 -0400

    Tutorial Voice Over Script Added
    Still need to implement and update sounds and naming conventions

---
+++ Assets/Scripts/TutorialVoiceOver.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TutorialVoiceOver : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip sound1;
+    public AudioClip sound2;
+    public AudioClip sound3;
+    public AudioClip sound4;
+    public AudioClip sound5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Collider1")
+        {
+            Debug.Log("Test 1");
+            audioSource.PlayOneShot(sound1);
+        }
+        if(other.tag == "Collider2")
+        {
+            Debug.Log("Test 2");
+            audioSource.PlayOneShot(sound2);
+        }
+        if(other.tag == "Collider3")
+        {
+            Debug.Log("Test 3");
+            audioSource.PlayOneShot(sound3);
+        }
+        if(other.tag == "Collider4")
+        {
+            Debug.Log("Test 4");
+            audioSource.PlayOneShot(sound4);
+        }
+        if(other.tag == "Collider5")
+        {
+            Debug.Log("Test 5");
+            audioSource.PlayOneShot(sound5);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Collider1")
+        {
+            Debug.Log("Collider disabled 1");
+            Destroy(GameObject.Find("nameOfCollider1"));
+        }
+        if(other.tag == "Collider2")
+        {
+            Debug.Log("Collider disabled 2");
+            Destroy(GameObject.Find("nameOfCollider2"));
+        }
+        if(other.tag == "Collider3")
+        {
+            Debug.Log("Collider disabled 3");
+            Destroy(GameObject.Find("nameOfCollider3"));
+        }
+        if(other.tag == "Collider4")
+        {
+            Debug.Log("Collider disabled 4");
+            Destroy(GameObject.Find("nameOfCollider4"));
+        }
+        if(other.tag == "Collider5")
+        {
+            Debug.Log("Collider disabled 5");
+            Destroy(GameObject.Find("nameOfCollider5"));
+        }
+        
+    }
+}
