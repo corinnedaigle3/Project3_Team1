02f299c2f45b2e4f9ad3b8edf9955f89bcb8ed03
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-04-23 13:05:05 -0400

    Tutorial Voiceover code is now a switch statement

--- Assets/Scripts/TutorialVoiceOver.cs
+++ Assets/Scripts/TutorialVoiceOver.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TutorialVoiceOver : MonoBehaviour
@@ -11,23 +10,47 @@ public class TutorialVoiceOver : MonoBehaviour
     public AudioClip sound3;
     public AudioClip sound4;
     public AudioClip sound5;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
-        
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Collider1")
+        switch (other.tag)
+        {
+            case "Collider1":
+                Debug.Log("Test 1");
+                audioSource.PlayOneShot(sound1);
+                break;
+
+            case "Collider2":
+                Debug.Log("Test 2");
+                audioSource.PlayOneShot(sound2);
+                break;
+
+            case "Collider3":
+                Debug.Log("Test 3");
+                audioSource.PlayOneShot(sound3);
+                break;
+
+            case "Collider4":
+                Debug.Log("Test 4");
+                audioSource.PlayOneShot(sound4);
+                break;
+
+            case "Collider5":
+                Debug.Log("Test 5");
+                audioSource.PlayOneShot(sound5);
+                break;
+
+            default:
+                break;
+        }
+
+        /*if (other.tag == "Collider1")
         {
             Debug.Log("Test 1");
             audioSource.PlayOneShot(sound1);
@@ -51,12 +74,43 @@ public class TutorialVoiceOver : MonoBehaviour
         {
             Debug.Log("Test 5");
             audioSource.PlayOneShot(sound5);
-        }
+        }*/
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(other.tag == "Collider1")
+        switch (other.tag)
+        {
+            case "Collider1":
+                Debug.Log("Collider disabled 1");
+                Destroy(GameObject.Find("nameOfCollider1"));
+                break;
+
+            case "Collider2":
+                Debug.Log("Collider disabled 2");
+                Destroy(GameObject.Find("nameOfCollider2"));
+                break;
+
+            case "Collider3":
+                Debug.Log("Collider disabled 3");
+                Destroy(GameObject.Find("nameOfCollider3"));
+                break;
+
+            case "Collider4":
+                Debug.Log("Collider disabled 4");
+                Destroy(GameObject.Find("nameOfCollider4"));
+                break;
+
+            case "Collider5":
+                Debug.Log("Collider disabled 5");
+                Destroy(GameObject.Find("nameOfCollider5"));
+                break;
+
+            default:
+                break;
+        }
+
+        /*if (other.tag == "Collider1")
         {
             Debug.Log("Collider disabled 1");
             Destroy(GameObject.Find("nameOfCollider1"));
@@ -80,7 +134,7 @@ public class TutorialVoiceOver : MonoBehaviour
         {
             Debug.Log("Collider disabled 5");
             Destroy(GameObject.Find("nameOfCollider5"));
-        }
+        }*/
         
     }
 }
